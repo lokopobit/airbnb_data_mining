82a856d337403b2918448f857f259799a01ad6ac
Manuel Rafael Vazquez Gandullo <manuelrv@ucm.es>
2020-05-06 21:13:15 +0200

    Add: more classification models and 1 ordinal model.
    ModellingCat.R: bagFDAGCV, bagFDA, ada, LogitBoost, J48, C5.0
    
    ModellingOrdinal.R: rpartScore

--- ModellingCat.R
+++ ModellingCat.R
@@ -10,6 +10,12 @@ library(doParallel)
 trellis.par.set(caretTheme())
 
 # Create binary variable
+a <- clean_data
+a$binPrice <- ifelse(clean_data$price <= mean(clean_data$price), 0, 1)
+a$binPrice[a$beds > 1] <- 2
+a$binPrice <- factor(a$binPrice)
+a$price <- NULL
+
 clean_data$binPrice <- factor(ifelse(clean_data$price <= mean(clean_data$price), 0, 1))
 
 clean_data$price <- NULL
@@ -33,9 +39,10 @@ fitControl <- trainControl(method = "repeatedcv", number = 10, repeats = 1)
 ####################### MODELS WITHOUT HYPERPARAMETERS #####################
 ############################################################################
 
-model.Grid <-  expand.grid(mfinal = c(3, 6), maxdepth = c(5, 10))
-model.Fit <- train(fmla, data = a, 
-                   method = "AdaBag", 
+
+model.Grid <-  expand.grid(cp = seq(100,500,100), split = c('abs', 'quad'), prune = c('mr', 'mc'))
+model.Fit <- train(fmla, data = a, #dataTrain
+                   method = "chaid", 
                    trControl = fitControl,
                    tuneGrid = model.Grid)
 
@@ -125,9 +132,10 @@ target.catMult.NoHyper <- function(fmla, dataTrain, fitcontrol, parallel = TRUE,
 
 ###################### BINARY CLASSIFICATION ONLY ##########################
 target.catBin.Hyper <- function(fmla, dataTrain, fitcontrol, parallel = TRUE, slow = FALSE) {
-  # 
+  # ada: BOOSTED CLASSIFICATION TREES: https://cran.r-project.org/web/packages/ada/ : https://cran.r-project.org/web/packages/plyr/
 
-  fast.models <- c("")
+  fast.models <- c("ada")
+  ada.Grid <-  expand.grid(iter = 100, maxdepth = c(4, 6), nu = 0.5)
 
   
   # ADABOOST: ADABOOST CLASSIFICATION TREES: https://cran.r-project.org/web/packages/fastAdaboost/
@@ -177,15 +185,24 @@ target.catBin.Hyper <- function(fmla, dataTrain, fitcontrol, parallel = TRUE, sl
 target.catMult.Hyper <- function(fmla, dataTrain, fitcontrol, parallel = TRUE, slow = FALSE) {
   # ADABAG: BAGGED ADABOOST: https://cran.r-project.org/web/packages/adabag/
   # ADABOOST.M1: ADABOOST.M1: https://cran.r-project.org/web/packages/adabag/
+  # bagFDAGCV: BAGGED FDA USING gCV PRUNING: https://cran.r-project.org/web/packages/earth/
+  # LogitBoost: BOOSTED LOGISTIC REGRESSION: https://cran.r-project.org/web/packages/caTools/
+  # J48: C4.5-LIKE TREES: https://cran.r-project.org/web/packages/RWeka/
+  # C5.0: C5.0: https://cran.r-project.org/web/packages/C50/
   
-  fast.models <- c("AdaBag", "AdaBoost.M1")
+  fast.models <- c("AdaBag", "AdaBoost.M1", "bagFDAGCV", "LogitBoost", "J48", "C5.0")
   AdaBag.Grid <-  expand.grid(mfinal = c(3, 6), maxdepth = c(5, 10))
   AdaBoost.M1.Grid <-  expand.grid(mfinal = c(3, 6), maxdepth = c(5, 10), coeflearn = "Zhu")
+  bagFDAGCV.Grid <-  expand.grid(degree = c(1, 2))
+  LogitBoost.Grid <-  expand.grid(nIter = seq(100,300,100))
+  J48.Grid <-  expand.grid(C = seq(0.01,0.05,0.01), M = 5)
+  C5.0.Grid <-  expand.grid(trials = seq(20,100,20), model = c('rules', 'tree'), winnow = c(TRUE,FALSE))
   
-  # 
   
-  slow.models <- c("")
-
+  # bagFDA: BAGGED FLEXIBLE DISCRIMINANT ANALYSIS: https://cran.r-project.org/web/packages/earth/ : https://cran.r-project.org/web/packages/mda/
+  
+  slow.models <- c("bagFDA")
+  bagFDA.Grid <-  expand.grid(degree = c(1, 2), nprune = c (1,2))
   
   if (parallel) {
     cl <- makePSOCKcluster(3)
@@ -230,11 +247,19 @@ target.catMult.Hyper <- function(fmla, dataTrain, fitcontrol, parallel = TRUE, s
 ####################### MODELS NOT WORKING  ################################
 ############################################################################
 
-model.Grid <-  expand.grid(nIter = seq(100, 150, 25), method = "Adaboost.M1")
-model.Fit <- train(fmla, data = a, 
-                   method = "adaboost", 
+
+
+
+##########################################################################
+###################### SPECIAL MODELS ####################################
+##########################################################################
+
+# Train data must be 0 or 1
+model.Grid <-  expand.grid(lambda.freqs = c(0.1, 0.2))
+model.Fit <- train(fmla, data = dataTrain, #dataTrain
+                   method = "binda", 
                    trControl = fitControl,
                    tuneGrid = model.Grid)
 
 plot(model.Fit)
-print(model.Fit$results)
+model.Fit
--- ModellingOrdinal.R
+++ ModellingOrdinal.R
@@ -1,2 +1,6 @@
 
 # VGLMADJCAT: ADJACENT CATEGORIES PROBABILITY MODEL FOR ORDINAL DATA: https://cran.r-project.org/web/packages/VGAM/
+# rpartScore: CART OR ORDINAL RESPONSES: https://cran.r-project.org/web/packages/rpartScore/
+rpartScore.Grid <-  expand.grid(cp = seq(100,500,100), split = c('abs', 'quad'), prune = c('mr', 'mc'))
+
+# 
