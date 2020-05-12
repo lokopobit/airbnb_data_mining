4b9b0b012ef996f9edd483381cf08f67accf948e
Manuel Rafael Vazquez Gandullo <manuelrv@ucm.es>
2020-05-12 21:19:15 +0200

    Add: more calssification models and 1 auxiliary function.
    ModellingCat.R: qda, stepQDA, rFerns, rda, rlda, regLogistic, Linda, rmda and QdaCov.
    
    ModellingOrdinal.R: ordinalNet and ordinalRF.
    
    An auxiliary function dedicated to model fitting was created.
    
    Some models that were classified as binary classifers have been moved to multinomial classifiers (giver its capabilities).
    
    todo.txt updated also.

--- ModellingCat.R
+++ ModellingCat.R
@@ -39,34 +39,21 @@ fitControl <- trainControl(method = "repeatedcv", number = 10, repeats = 1)
 ####################### MODELS WITHOUT HYPERPARAMETERS #####################
 ############################################################################
 
-model.Grid <-  expand.grid(decay = seq(0.1,0.9,0.3))
-model.Fit <- train(fmla, data = dataTrain, #dataTrain
-                   method = "multinom", 
+model.Grid <-  expand.grid(K = seq(2,10,1), model = c("EII", "VII", "EEI", "EVI", "VEI", "VVI"))
+model.Grid <-  expand.grid(K = 2, model = c("EII"))
+model.Fit <- train(fmla, data = a, #dataTrain
+                   method = "QdaCov", 
                    trControl = fitControl,
                    tuneGrid = model.Grid)
-getModelInfo('multinom')
+getModelInfo('rmda')
 plot(model.Fit)
 model.Fit
-
-
-model.Grid <-  expand.grid(maxvar = c(10,30), direction = c("both", "forward", "backward"))
-garbage <- capture.output(model.Fit <- train(fmla, data = a, #dataTrain
-                                             method = "stepLDA", 
-                                             trControl = fitControl,
-                                             tuneGrid = model.Grid))
-
-getModelInfo('stepLDA')
-plot(model.Fit)
-model.Fit
-
 
 
 ###################### BINARY CLASSIFICATION ONLY ##########################
 target.catBin.NoHyper <- function(fmla, dataTrain, fitcontrol, parallel = TRUE, slow = FALSE) {
-  # lda: LINEAR DISCRIMINANT ANALYSIS: https://cran.r-project.org/web/packages/MASS/
-  # Mlda: MAXIMUN UNCERTAINTY LINEAR DISCRIMINANT ANALYSIS: https://cran.r-project.org/web/packages/HiDimDA/
-  
-  fast.models <- c("lda", "Mlda")
+  # 
+  fast.models <- c("")
   
   # 
   slow.models <- c("")
@@ -103,8 +90,13 @@ target.catBin.NoHyper <- function(fmla, dataTrain, fitcontrol, parallel = TRUE, 
 
 ###################### MULTICLASS CLASSIFICATION #############################
 target.catMult.NoHyper <- function(fmla, dataTrain, fitcontrol, parallel = TRUE, slow = FALSE) {
-  # 
-  fast.models <- c("")
+  # lda: LINEAR DISCRIMINANT ANALYSIS: https://cran.r-project.org/web/packages/MASS/
+  # Mlda: MAXIMUN UNCERTAINTY LINEAR DISCRIMINANT ANALYSIS: https://cran.r-project.org/web/packages/HiDimDA/
+  # qda: QUADRATIC DISCRIMINANT ANALYSIS: https://cran.r-project.org/web/packages/MASS/
+  # Linda: ROBUST LINEAR DISCRIMINANT ANALYSIS: https://cran.r-project.org/web/packages/rrcov/
+  # QdaCov: ROBUST QUADRATIC DISCRIMINANT ANALYSIS: https://cran.r-project.org/web/packages/rrcov/
+  
+  fast.models <- c("lda", "Mlda", "qda", "Linda", "QdaCov")
   
   # 
   slow.models <- c("")
@@ -145,62 +137,24 @@ target.catMult.NoHyper <- function(fmla, dataTrain, fitcontrol, parallel = TRUE,
 ############################################################################
 
 ###################### BINARY CLASSIFICATION ONLY ##########################
-target.catBin.Hyper <- function(fmla, dataTrain, fitcontrol, parallel = TRUE, slow = FALSE) {
+target.catBin.Hyper <- function(fmla, dataTrain, fitControl, parallel = TRUE, slow = FALSE) {
   # ada: BOOSTED CLASSIFICATION TREES: https://cran.r-project.org/web/packages/ada/ : https://cran.r-project.org/web/packages/plyr/
   # C5.0Cost: COST-SENSITIVE C5.0: https://cran.r-project.org/web/packages/C50/ : https://cran.r-project.org/web/packages/plyr/
   # rpartCost: COST-SENSITIVE CART: https://cran.r-project.org/web/packages/rpart/ : https://cran.r-project.org/web/packages/plyr/
   # deepboost: DEEPBOOST: https://cran.r-project.org/web/packages/deepboost/
-  # RFlda: FACTOR-BASED LINEAR DISCRIMINANT ANALYSIS: https://cran.r-project.org/web/packages/HiDimDA/
-  # fda: FLEXIBLE DISCRIMINANT ANALYSIS: https://cran.r-project.org/web/packages/earth/ : https://cran.r-project.org/web/packages/mda/
-  # protoclass: GREEDY PROPOTYPE SELECTION: https://cran.r-project.org/web/packages/protoclass/ : https://cran.r-project.org/web/packages/proxy/
-  # hda: HETEROSCEDASTIC DISCRIMINANT ANALYSIS: https://cran.r-project.org/web/packages/hda/
-  # hdda: HIGH DIMENSIONAL DISCRIMINANT ANALYSIS: https://cran.r-project.org/web/packages/HDclassif/
   # svmLinearWeights2: L2 REGULARIZED LINEAR SUPPORT VECTOR MACHINES WITH CLASS WEIGHTS: https://cran.r-project.org/web/packages/LiblineaR/
-  # lvq: LEARNING VECTOR QUANTIZATION: https://cran.r-project.org/web/packages/class/
-  # lssvmRadial: LEAST SQUARES SUPPORT VECTOR MACHINE WITH RADIAL BASIS FUNCTION KERNEL: https://cran.r-project.org/web/packages/kernlab/
-  # lda2: LINEAR DISCRIMINANT ANALYSIS: https://cran.r-project.org/web/packages/MASS/
-  # stepLDA: LINEAR DISCRIMINANT ANALYSIS WITH STEPWISE FEATURE SELECTION: https://cran.r-project.org/web/packages/MASS/ : https://cran.r-project.org/web/packages/klaR/
-  # dwdLinear: LINEAR DISTANCE WEIGHTED DISCRIMINATION: https://cran.r-project.org/web/packages/kerndwd/
   # svmLinearWeights: LINEAR SUPPORT VECTOR MACHINES WITH CLASS WEIGHTS: https://cran.r-project.org/web/packages/e1071/
-  # LMT: LOGISTIC MODEL TREES: https://cran.r-project.org/web/packages/RWeka/
-  # mda: MIXTURE DISCRIMINANT ANALYSIS: https://cran.r-project.org/web/packages/mda/
-  # naive_bayes: NAIVE BAYES: https://cran.r-project.org/web/packages/naivebayes/
-  # nb: NAIVE BAYES: https://cran.r-project.org/web/packages/klaR/
-  # pam: NEAREST SHRUNKEN CENTROIDS: https://cran.r-project.org/web/packages/pamr/
-  # ownn: OPTIMAL WEIGHTED NEAREST NEIGHBOR CLASSIFIER: https://cran.r-project.org/web/packages/snn/
   # PRIM: PATIENT RULE INDUCTION METHOD: https://cran.r-project.org/web/packages/supervisedPRIM/
-  # pda: PENALIZED DISCRIMINANT ANALYSIS: https://cran.r-project.org/web/packages/mda/
-  # pda: PENALIZED DISCRIMINANT ANALYSIS: https://cran.r-project.org/web/packages/mda/
-  
-  fast.models <- c("ada", "C5.0Cost", "rpartCost", "deepboost", "RFlda", "fda", "protoclass", "hda",
-                   "hdda", "svmLinearWeights2", "lvq", "lssvmRadial", "lda2", "stepLDA", "dwdLinear",
-                   "svmLinearWeights", "LMT", "mda", "naive_bayes", "nb", "pam", "ownn", "PRIM",
-                   "pda", "pda2")
+
+  fast.models <- c("ada", "C5.0Cost", "rpartCost", "deepboost", "svmLinearWeights2","svmLinearWeights", 
+                   "PRIM")
   ada.Grid <-  expand.grid(iter = 100, maxdepth = c(4, 6), nu = 0.5)
   C5.0Cost.Grid <-  expand.grid(trials = seq(10,30,10), model = c("tree", "rules"), winnow = c(TRUE, FALSE), cost = 1:3)
   rpartCost.Grid <-  expand.grid(cp = 1:3, Cost = 1:3)
   deepboost.Grid <-  expand.grid(num_iter = seq(10,30,20), tree_depth = 5:6, beta = seq(0.2,0.3,0.1), lambda = 0.3, loss_type = "l")
-  RFlda.Grid <-  expand.grid(q = 2:4)
-  fda.Grid <-  expand.grid(degree = 2:4, nprune = c(5,10))
-  protoclass.Grid <-  expand.grid(eps = 50, Minkowski = 1)
-  hda.Grid <-  expand.grid(gamma = seq(0.1,0.9,0.3), lambda = seq(0.2,0.9,0.3), newdim = c(2, 5, 10))
-  hdda.Grid <-  expand.grid(threshold = seq(0.1,0.9,0.3), model = c("AkjBkQkDk", "AkBkQkDk", "ABkQkDk"))
   svmLinearWeights2.Grid <-  expand.grid(cost = c(0.1,0.9), Loss = c("L1","L2"), weight = c(5,15))
-  lvq.Grid <-  expand.grid(size = c(1,2), k = c(5,15))
-  lssvmRadial.Grid <-  expand.grid(sigma = seq(1,10,6), tau = seq(0.1,0.9,0.6))
-  lda2.Grid <-  expand.grid(dimen = seq(1,30,3))
-  stepLDA.Grid <-  expand.grid(maxvar = c(10,30), direction = c("both", "forward", "backward"))
-  dwdLinear.Grid <-  expand.grid(lambda = seq(0.2,0.9,0.3), qval = c(5, 10))
   svmLinearWeights.Grid <-  expand.grid(cost = seq(2,9,3), weight = c(2, 4))
-  LMT.Grid <-  expand.grid(iter = c(3, 10))
-  mda.Grid <-  expand.grid(subclasses = c(3, 10))
-  naive_bayes.Grid <-  expand.grid(usekernel = c(TRUE, FALSE), laplace = seq(0.1,0.9,0.2), adjust = seq(0.1,1,0.2))
-  nb.Grid <-  expand.grid(usekernel = c(TRUE, FALSE), fL = seq(0.1,0.9,0.2), adjust = seq(0.1,1,0.2))
-  pam.Grid <-  expand.grid(threshold = seq(0.1,1,0.22))
-  ownn.Grid <-  expand.grid(K = c(2,5))
   PRIM.Grid <-  expand.grid(peel.alpha = seq(0.01,0.25,0.09), paste.alpha = seq(0.01,0.25,0.2), mass.min = seq(0.01,0.25,0.2))
-  pda.Grid <-  expand.grid(lambda = seq(0.01,0.9,0.1))
-  pda2.Grid <-  expand.grid(df = seq(1,20,10))
   
   # ADABOOST: ADABOOST CLASSIFICATION TREES: https://cran.r-project.org/web/packages/fastAdaboost/
   # ORFlog: OBLIQUE RANDOM FOREST: https://cran.r-project.org/web/packages/obliqueRF/
@@ -225,25 +179,7 @@ target.catBin.Hyper <- function(fmla, dataTrain, fitcontrol, parallel = TRUE, sl
   models.Results <- list()
   for (model in fast.models) {
     model.Grid <- eval(parse(text = paste(model, '.Grid', sep='')))
-    tryCatch({message(paste(rep('-', 20), collapse = ''))
-              message(paste('TRYING:', model, sep = ' '))
-              garbage <- capture.output(model.Fit <- train(fmla, data = dataTrain, 
-                       method = model, 
-                       trControl = fitControl,
-                       tuneGrid = model.Grid))
-              print(model.Grid)
-              print(model.Fit$results)},
-             warning = function(cond) {
-               message('WARNING:')
-               message(cond)
-               return(NA)},
-              error = function(cond) {
-                message('ERROR:')
-                message(cond)
-                message(paste(rep('-', 20), collapse = ''))
-                return(NA)},
-             finally = {
-               message(paste(model, 'FINISHED',sep = ' '))})
+    fitting(model.Grid, model, fmla, dataTrain, fitControl)
     #models.Results <- rbind(models.Results, model.Fit$results)
     #plot(model.Fit)
     #print(model.Fit$results)
@@ -279,9 +215,33 @@ target.catMult.Hyper <- function(fmla, dataTrain, fitcontrol, parallel = TRUE, s
   # J48: C4.5-LIKE TREES: https://cran.r-project.org/web/packages/RWeka/
   # C5.0: C5.0: https://cran.r-project.org/web/packages/C50/
   # multinom: PENALIZED MULTINOMIAL REGRESSION: https://cran.r-project.org/web/packages/nnet/
+  # RFlda: FACTOR-BASED LINEAR DISCRIMINANT ANALYSIS: https://cran.r-project.org/web/packages/HiDimDA/
+  # fda: FLEXIBLE DISCRIMINANT ANALYSIS: https://cran.r-project.org/web/packages/earth/ : https://cran.r-project.org/web/packages/mda/
+  # protoclass: GREEDY PROPOTYPE SELECTION: https://cran.r-project.org/web/packages/protoclass/ : https://cran.r-project.org/web/packages/proxy/
+  # hda: HETEROSCEDASTIC DISCRIMINANT ANALYSIS: https://cran.r-project.org/web/packages/hda/
+  # hdda: HIGH DIMENSIONAL DISCRIMINANT ANALYSIS: https://cran.r-project.org/web/packages/HDclassif/
+  # lvq: LEARNING VECTOR QUANTIZATION: https://cran.r-project.org/web/packages/class/
+  # lssvmRadial: LEAST SQUARES SUPPORT VECTOR MACHINE WITH RADIAL BASIS FUNCTION KERNEL: https://cran.r-project.org/web/packages/kernlab/
+  # lda2: LINEAR DISCRIMINANT ANALYSIS: https://cran.r-project.org/web/packages/MASS/
+  # stepLDA: LINEAR DISCRIMINANT ANALYSIS WITH STEPWISE FEATURE SELECTION: https://cran.r-project.org/web/packages/MASS/ : https://cran.r-project.org/web/packages/klaR/
+  # dwdLinear: LINEAR DISTANCE WEIGHTED DISCRIMINATION: https://cran.r-project.org/web/packages/kerndwd/ 
+  # LMT: LOGISTIC MODEL TREES: https://cran.r-project.org/web/packages/RWeka/
+  # mda: MIXTURE DISCRIMINANT ANALYSIS: https://cran.r-project.org/web/packages/mda/
+  # naive_bayes: NAIVE BAYES: https://cran.r-project.org/web/packages/naivebayes/
+  # nb: NAIVE BAYES: https://cran.r-project.org/web/packages/klaR/
+  # pam: NEAREST SHRUNKEN CENTROIDS: https://cran.r-project.org/web/packages/pamr/
+  # ownn: OPTIMAL WEIGHTED NEAREST NEIGHBOR CLASSIFIER: https://cran.r-project.org/web/packages/snn/
+  # pda: PENALIZED DISCRIMINANT ANALYSIS: https://cran.r-project.org/web/packages/mda/
+  # pda: PENALIZED DISCRIMINANT ANALYSIS: https://cran.r-project.org/web/packages/mda/
+  # stepQDA: QUADRATIC DISCRIMINANT ANALYSIS WITH STEPWISE FEATURE SELECTION: https://cran.r-project.org/web/packages/MASS/ : https://cran.r-project.org/web/packages/klaR/
+  # rFerns: RANDOM FERNS: https://cran.r-project.org/web/packages/rFerns/: 
+  # rda: REGURALIZED DISCRIMINANT ANALYSIS: https://cran.r-project.org/web/packages/klaR/
+  # regLogistic: REGULARIZED LOGISTIC REGRESSION: https://cran.r-project.org/web/packages/LiblineaR/
   
-  
-  fast.models <- c("AdaBag", "AdaBoost.M1", "bagFDAGCV", "LogitBoost", "J48", "C5.0", "multinom")
+  fast.models <- c("AdaBag", "AdaBoost.M1", "bagFDAGCV", "LogitBoost", "J48", "C5.0", "multinom",
+                   "RFlda", "fda", "protoclass", "hda", "hdda", "lvq", "lssvmRadial", "lda2", 
+                   "stepLDA", "dwdLinear", "LMT", "mda", "naive_bayes", "nb", "pam", "ownn",
+                   "pda", "pda2", "stepQDA", "rFerns", "rda", "regLogistic")
   AdaBag.Grid <-  expand.grid(mfinal = c(3, 6), maxdepth = c(5, 10))
   AdaBoost.M1.Grid <-  expand.grid(mfinal = c(3, 6), maxdepth = c(5, 10), coeflearn = "Zhu")
   bagFDAGCV.Grid <-  expand.grid(degree = c(1, 2))
@@ -289,15 +249,39 @@ target.catMult.Hyper <- function(fmla, dataTrain, fitcontrol, parallel = TRUE, s
   J48.Grid <-  expand.grid(C = seq(0.01,0.05,0.01), M = 5)
   C5.0.Grid <-  expand.grid(trials = seq(20,100,20), model = c('rules', 'tree'), winnow = c(TRUE,FALSE))
   multinom.Grid <-  expand.grid(decay = seq(0.1,0.9,0.3))
+  RFlda.Grid <-  expand.grid(q = 2:4)
+  fda.Grid <-  expand.grid(degree = 2:4, nprune = c(5,10))
+  protoclass.Grid <-  expand.grid(eps = 50, Minkowski = 1)
+  hda.Grid <-  expand.grid(gamma = seq(0.1,0.9,0.3), lambda = seq(0.2,0.9,0.3), newdim = c(2, 5, 10))
+  hdda.Grid <-  expand.grid(threshold = seq(0.1,0.9,0.3), model = c("AkjBkQkDk", "AkBkQkDk", "ABkQkDk"))
+  lvq.Grid <-  expand.grid(size = c(1,2), k = c(5,15))
+  lssvmRadial.Grid <-  expand.grid(sigma = seq(1,10,6), tau = seq(0.1,0.9,0.6))
+  lda2.Grid <-  expand.grid(dimen = seq(1,30,3))
+  stepLDA.Grid <-  expand.grid(maxvar = ncol(dataTrain), direction = c("both", "forward", "backward"))
+  dwdLinear.Grid <-  expand.grid(lambda = seq(0.2,0.9,0.3), qval = c(5, 10))
+  LMT.Grid <-  expand.grid(iter = c(3, 10))
+  mda.Grid <-  expand.grid(subclasses = c(3, 10))
+  naive_bayes.Grid <-  expand.grid(usekernel = c(TRUE, FALSE), laplace = seq(0.1,0.9,0.2), adjust = seq(0.1,1,0.2))
+  nb.Grid <-  expand.grid(usekernel = c(TRUE, FALSE), fL = seq(0.1,0.9,0.2), adjust = seq(0.1,1,0.2))
+  pam.Grid <-  expand.grid(threshold = seq(0.1,1,0.22))
+  ownn.Grid <-  expand.grid(K = c(2,5))
+  pda.Grid <-  expand.grid(lambda = seq(0.01,0.9,0.1))
+  pda2.Grid <-  expand.grid(df = seq(1,20,10))
+  stepQDA.Grid <-  expand.grid(maxvar = ncol(dataTrain), direction = 'both')
+  rFerns.Grid <-  expand.grid(depth = c(1,8))
+  rda.Grid <-  expand.grid(gamma = seq(0.1, 1, 0.3), lambda =  seq(0.1, 1, 0.3))
+  regLogistic.Grid <-  expand.grid(cost = seq(0.1, 0.9, 0.3), loss = c("L1", "L2_dual", "L2_primal"), epsilon = 0.01)
   
   # bagFDA: BAGGED FLEXIBLE DISCRIMINANT ANALYSIS: https://cran.r-project.org/web/packages/earth/ : https://cran.r-project.org/web/packages/mda/
   # dwdPoly: DISTANCE WEIGHTED DISCRIMINATION WITH POLYNOMIAL KERNEL: https://cran.r-project.org/web/packages/kerndwd/
   # dwdRadial: DISTANCE WEIGHTED DISCRIMINATION WITH RADIAL BASIS FUNCTION KERNEL: https://cran.r-project.org/web/packages/kerndwd/ : https://cran.r-project.org/web/packages/kernlab/ 
+  # rmda: ROBUST MIXTURE DISCRIMINANT ANALYSIS: https://cran.r-project.org/web/packages/robustDA/index.html
   
-  slow.models <- c("bagFDA", "dwdPoly", "dwdRadial")
+  slow.models <- c("bagFDA", "dwdPoly", "dwdRadial", "rmda")
   bagFDA.Grid <-  expand.grid(degree = c(1, 2), nprune = c (1,2))
   dwdPoly.Grid <-  expand.grid(lambda = 0.1, qval = 1, degree = 1, scale = 1)
   dwdRadial.Grid <-  expand.grid(lambda = 0.1, qval = 1, sigma = 4)
+  rmda.Grid <-  expand.grid(K = seq(2,10,1), model = c("EII", "VII", "EEI", "EVI", "VEI", "VVI"))
   
   if (parallel) {
     cl <- makePSOCKcluster(3)
@@ -431,15 +415,23 @@ plot(model.Fit)
 model.Fit
 
 
-
-
-
-
-# to multiclass: fda, protoclass, hda, hdda, lssvmRadial, lda2, stepLDA, dwdLinear, LMT, mda, naive_bayes, pam, ownn, pda, pda2.
-
-
-
-
-
-
+# Auxiliary function for fitting
+fitting <- function(model.Grid, model, fmla, dataTrain, fitControl) {
+tryCatch({message(paste(rep('-', 20), collapse = ''))
+  st <- Sys.time()
+  message(paste('TRYING:', model, sep = ' '))
+  garbage <- capture.output(model.Fit <- train(fmla, data = dataTrain, 
+                                               method = model, 
+                                               trControl = fitControl,
+                                               tuneGrid = model.Grid))
+  print(model.Fit$results)
+  et <- Sys.time()
+  print(et-st)},
+  error = function(cond) {
+    message('ERROR:')
+    message(cond)
+    return(NA)},
+  finally = {
+    message(paste(model, 'FINISHED',sep = ' '))})
+}
 
--- ModellingOrdinal.R
+++ ModellingOrdinal.R
@@ -9,3 +9,7 @@ model.Grid <-  expand.grid(parallel = c(TRUE, FALSE), link = c('probit'))
 # vglmCumulative: CUMULATIVE PROBABILITY MODEL FOR ORDINAL DATA: https://cran.r-project.org/web/packages/VGAM/
 
 # polr: ORDERED LOGISTIC OR PROBIT REGRESSION: https://cran.r-project.org/web/packages/MASS/
+
+# ordinalNet: PENALIZED ORDINAL REGRESSION: https://cran.r-project.org/web/packages/ordinalNet/
+
+# ordinalRF: RANDOM FOREST: https://cran.r-project.org/web/packages/ordinalForest/ : https://cran.r-project.org/web/packages/ranger/
